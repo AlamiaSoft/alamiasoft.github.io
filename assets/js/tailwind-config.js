5640d4b41c965a667a46b3da1e403f6c8f758b1b
Ali Raza <amr.shah@gmail.com>
2025-10-18 05:03:56 -0700

    Added website files via upload
    Added website files via upload

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'alamia-black': '#000000',
+                        'alamia-white': '#FFFFFF',
+                        'alamia-gray': '#F5F5F5',
+                        'alamia-light-gray': '#F0F0F0',
+                        'alamia-dark-gray': '#111827',
+                        'alamia-green': '#36b82al',
+                        'alamia-orange': '#fcb63d',
+                        'alamia-blue': '#0066CC',
+                        'alamia-light': '#E6F2FF',
+                        'alamia-dark': '#003D7A',
+                    },
+                    fontFamily: {
+                        ubuntu: ['Ubuntu', 'system-ui', 'sans-serif'],
+                        nunito: ['Nunito', 'system-ui', 'sans-serif'],
+                    }
+                }
+            }
+        }
